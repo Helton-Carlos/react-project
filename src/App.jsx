b6877e73b21aa5cd0cf9cd5bab1a0bf3715eba11
Helton Brito <heltoncarlospa@hotmail.com>
2022-11-11 23:39:47 -0300

    fix: testing get mirage js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import CardComponent from "./components/CardComponent";
+import { useState, useEffect } from "react";
+
 function App() {
+  let [reminders, setReminders] = useState([])
+
+  useEffect(() => {
+    fetch("/api/reminders")
+      .then((res) => res.json())
+      .then((json) => {
+        setReminders(json.reminders)
+      })
+  }, [])
   return (
     <>
+      <ul>
+        {reminders.map((reminders) => (
+          <li key={reminders.id}>{reminders.text}</li>
+        ))}
+      </ul>
       <CardComponent />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Server } from 'miragejs'
+
+new Server({
+  routes() {
+    this.namespace = "api"
+    this.get('/reminders', () => ({
+      reminders: [
+        { id: 1, text: 'Walk the dog' },
+        { id: 2, text: 'Take out the trash' },
+        { id: 3, text: 'Work out' },
+      ],
+    }))
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
