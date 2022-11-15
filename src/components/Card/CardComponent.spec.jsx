37cfe350494486f29a2cd2a2f9c43093dec1acce
Helton Brito <heltoncarlospa@hotmail.com>
2022-11-15 00:48:03 -0300

    feat: logo

--- src/components/Card/CardComponent.spec.jsx
+++ src/components/Card/CardComponent.spec.jsx
@@ -1,5 +1,12 @@
-describe('Jest', () => {
-  it('testing jest', () => {
-    expect(1).toBe(1);
-  });
-});
+import '@testing-library/jest-dom'
+import {render} from '@testing-library/react'
+import CardComponent from './CardComponent'
+
+describe('CardComponent component', () => {
+  test('Testing Component', () => {
+    const { getByText } = render(<CardComponent />)
+  
+    expect(getByText('React Tailwind Card with Image')).toBeTy()
+  })
+})
+
